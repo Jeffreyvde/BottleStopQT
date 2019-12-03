39109bd211790889f439d430ae5ab5bc9ee3a0f0
Stijn Eijndhoven <s.eijndhoven@outlook.com>
2019-12-03 15:22:40 +0100

    Add initialization for WiningPi

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include "mainwindow.h"
 #include <QApplication>
+#include <wiringPi.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
+
+    wiringPiSetup();
+
     w.show();
 
     return a.exec();
