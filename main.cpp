e2e96ed1143ced9f3540a2b86c55ec5eca14fa79
Jeffreyvde <jeffrey.vd.elshout@gmail.com>
2019-12-05 10:04:51 +0100

    Implement serial port in main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,34 @@
-#include "mainwindow.h"
-#include <QApplication>
+#include <QCoreApplication>
+#include <iostream>
+#include <QtSerialPort/QSerialPort>
+#include <QDebug>
+#include <QElapsedTimer>
+#include "serialwrapper.h"
+
+SerialWrapper* serialPort;
+
+QSerialPort* setupSerialPort();
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
-    MainWindow w;
-    w.show();
+    QCoreApplication a(argc, argv);
+    serialPort = new SerialWrapper(setupSerialPort());
 
     return a.exec();
+}
+
+QSerialPort* setupSerialPort()
+{
+    QSerialPort* serial = new QSerialPort();
+    serial->setPortName("ttyUSB0");
+    serial->open(QIODevice::ReadWrite);
+
+    serial->setBaudRate(QSerialPort::Baud9600);
+    serial->setDataBits(QSerialPort::Data8);
+    serial->setParity(QSerialPort::NoParity);
+    serial->setStopBits(QSerialPort::OneStop);
+    serial->setFlowControl(QSerialPort::NoFlowControl);
+
+    while(!serial->isOpen()) serial->open(QIODevice::ReadWrite);
+    return serial;
 }
