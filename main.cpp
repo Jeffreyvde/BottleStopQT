269d8d40558893a2b0baeddd684e84a6c9b05676
Jeffreyvde <jeffrey.vd.elshout@gmail.com>
2019-12-06 09:37:48 +0100

    Add bottle handler basics

--- main.cpp
+++ main.cpp
@@ -3,16 +3,11 @@
 #include "mainwindow.h"
 #include "serialwrapper.h"
 
-QSerialPort* setupSerialPort();
-SerialWrapper* serialPort;
-
-
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
-    serialPort = new SerialWrapper();
 
     return a.exec();
 }
