c8fcfb8c470344b81d2537020c2757fb9f7c32da
x1029 <kenzo.oznek12@gmail.com>
2019-12-09 10:13:18 +0100

    added comments and optimized vector

--- beverage.cpp
+++ beverage.cpp
@@ -1,5 +1,6 @@
 #include "beverage.h"
 
+// Initialize id, name, cost per milliliter and set recipe
 Beverage::Beverage(QJsonObject beverageData)
 {
     id = beverageData["ID"].toInt();
@@ -7,9 +8,11 @@ Beverage::Beverage(QJsonObject beverageData)
     costPerML = beverageData["CostPerML"].toDouble();
 
     QJsonArray recipe = beverageData["Recipe"].toArray();
+    ratios.reserve(2);
     setRecipe(recipe);
 }
 
+// Initialize beverage recipe(mix)
 void Beverage::setRecipe(QJsonArray recipe)
 {
     for(int i = 0; i < recipe.size(); i++){
@@ -18,20 +21,23 @@ void Beverage::setRecipe(QJsonArray recipe)
         double ratio = mix["Ratio"].toDouble();
 
         MixRatio mixRatio(pumpName, ratio);
-        ratios.push_back(mixRatio);
+        ratios.emplace_back(mixRatio);
     }
 }
 
+// Return beverage name
 QString Beverage::getName()
 {
     return name;
 }
 
+// Return beverage cost per milliliter
 double Beverage::getCostPerML()
 {
     return costPerML;
 }
 
+// Return all available recipe(mix) for beverage
 std::vector<MixRatio> Beverage::getRatios()
 {
     return ratios;
