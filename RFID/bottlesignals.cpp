ebd11e0f90b17f5e16439add7138c7dbc85a0579
Jeffreyvde <jeffrey.vd.elshout@gmail.com>
2020-01-13 13:26:12 +0100

    Add bottle signals class

---
+++ RFID/bottlesignals.cpp
@@ -0,0 +1,25 @@
+#include "bottlesignals.h"
+#include "devicemanager.h"
+
+BottleSignals::BottleSignals(QObject *parent) : QObject(parent)
+{
+
+}
+
+//Emits the signal for placebottle
+void BottleSignals::placeBottle(QString ID)
+{
+   if(DeviceManager::getInstance().setUser(ID))
+       emit bottlePlaced();
+
+}
+
+// Emits the signal for removebottle
+void BottleSignals::removeBottle()
+{
+    for (auto const& mapIndex : DeviceManager::getInstance().getPumpMap())
+    {
+        mapIndex.second->deactivate();
+    }
+    emit bottleRemoved();
+}
---
+++ RFID/bottlesignals.h
@@ -0,0 +1,22 @@
+#ifndef BOTTLESIGNALS_H
+#define BOTTLESIGNALS_H
+
+#include <QObject>
+
+class BottleSignals : public QObject
+{
+    Q_OBJECT
+public:
+    explicit BottleSignals(QObject *parent = nullptr);
+
+    void placeBottle(QString ID);
+    void removeBottle();
+
+signals:
+    void bottlePlaced();
+    void bottleRemoved();
+
+public slots:
+};
+
+#endif // BOTTLESIGNALS_H
