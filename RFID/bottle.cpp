214890cdd49bb4fd993287d83d6fbc505f7d854e
Jeffreyvde <jeffrey.vd.elshout@gmail.com>
2020-01-13 16:30:13 +0100

    Add json constructor for bottle

--- RFID/bottle.cpp
+++ RFID/bottle.cpp
@@ -1,9 +1,23 @@
 #include "bottle.h"
+#include <QDebug>
+#include <QJsonValueRef>
+#include <QJsonArray>
 
 Bottle::Bottle(QString ID, int sizeML)
 {
     this->ID = ID;
     this->sizeML = sizeML;
+}
+
+Bottle::Bottle(QJsonObject json)
+{
+    ID = json["bottleId"].toString();
+
+
+    QJsonArray model = json["bottleModel"].toArray();
+    sizeML = model[0].toObject()["bottleSizeMl"].toInt();
+
+    qDebug() << sizeML;
 }
 
 int Bottle::getSizeML() const
