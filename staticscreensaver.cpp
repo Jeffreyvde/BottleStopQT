7ad134939ae700813cc4a22f65ab5414d751de3b
Stijn Eijndhoven <s.eijndhoven@outlook.com>
2020-01-15 15:15:19 +0100

    Implemenet font

--- staticscreensaver.cpp
+++ staticscreensaver.cpp
@@ -1,11 +1,15 @@
 #include "staticscreensaver.h"
 #include "ui_staticscreensaver.h"
+#include <QFontDatabase>
 
 StaticScreensaver::StaticScreensaver(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::StaticScreensaver)
 {
     ui->setupUi(this);
+
+    QFontDatabase::addApplicationFont(":/CustomFonts/CustomFonts/BEBAS.ttf");
+    ui->label->setFont(QFont("BEBAS", 74, QFont::Normal, false));
 }
 
 StaticScreensaver::~StaticScreensaver()
